8db543c0000a91ea1fca061a2314ca43b8d12bed
Zyssar <116215784+Zyssar@users.noreply.github.com>
2024-11-20 15:59:14 -0500

    Update alu.v

--- single_cycle/alu/alu.v
+++ single_cycle/alu/alu.v
@@ -9,9 +9,9 @@
 //         - 0001: Subtraction
 //         - 0010: Multiplication
 //         - 0011: Unsigned division
-//         - 0100: Bitwise AND
+//         - 0100: signed division
+//         - 0101: Bitwise AND
 //         - 0110: Bitwise OR
-//         - 0101: signed division
 //         - 0111: XOR
 //         - 1000: NOT
 //         - 1001: LSL
@@ -26,7 +26,7 @@
 module alu (
     input       [31:0] a,           // Operand A
     input       [31:0] b,           // Operand B
-    input       [ 2:0] ALUControl,  // ALU control signal
+    input       [ 3:0] ALUControl,  // ALU control signal
     output reg  [31:0] Result,      // ALU result
     output wire [ 3:0] ALUFlags     // ALU Flags: {Negative, Zero, Carry, Overflow}
 );
@@ -54,21 +54,32 @@ module alu (
   assign sdiv = (b_signed != 0) ? (a_signed / b_signed) : 32'd0;
 
   // Unsigned division handling
-  wire [31:0] udiv_result;
+  wire [31:0] udiv;
   assign udiv = (b != 0) ? (a / b) : 32'd0;
   
   /// MULTIPLICATION
-  wire [31:0] mul_result;
-  assign mul_result = (a == 0 || b==0) ? 32'd0 : (a*b);
+  wire [31:0] mul;
+  assign mul = (a == 0 || b==0) ? 32'd0 : (a*b);
   
   // ALU operation based on ALUControl
   always @(*) begin
-    case (ALUControl[2:0])
-      4'b000, 3'b001: Result = sum;  // Addition or Subtraction
-      4'b100:         Result = a & b;  // Bitwise AND
-      4'b110:         Result = a | b;  // Bitwise OR
-      4'b101:         Result = sdiv;  // signed division
-      4'b111:         Result = udiv;  // unsigned division
+    case (ALUControl[3:0])
+      4'b0000, 3'b001: Result = sum;  // Addition or Subtraction ADD/SUB
+      4'b0101:         Result = a & b;  // Bitwise AND 
+      4'b0110:         Result = a | b;  // Bitwise ORR
+      4'b0100:         Result = sdiv;  // signed division SDIV
+      4'b0011:         Result = udiv;  // unsigned division UDIV
+      4'b0010:         Result = mul;  // multiplication MUL
+      4'b0111:         Result = (a*(~b))+((~a)*b); //XOR
+      4'b1000:         Result = ~a; //NOT o negate para logica
+      4'b1001:         Result = a << b[4:0]; //LSL
+      4'b1010:         Result = a >> b[4:0]; //LSR
+      4'b1011:         Result = $signed(a) >> b[4:0]; // ASR
+      4'b1100:         Result = (a==b) ? 32'd1: 32'd0;//Igualdad para logica
+      4'b1101:         Result = (a>b) ? 32'd1: 32'd0; //Mayor para logica
+      4'b1110:         Result =  (a<b) ? 32'd1: 32'd0;//Menor para logica
+      4'b1111:         Result =  32'd0;//meme
+      
     endcase
   end
 
